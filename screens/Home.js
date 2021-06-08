e3f658bc12bbfd24ee485055f7327ef33986bcf1
AD-NAP <shafik.adam98@gmail.com>
2021-06-08 23:52:50 +0800

    basic timer

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { globalStyles} from '../styles/global';
-
-
-
-
+import { ThemeProvider, Button } from 'react-native-elements';
+import { globalStyles } from '../styles/global';
 
 export default function Home({ navigation }) {
-    return(
+    const theme = {
+        Button: {
+          raised: true,
+          containerStyle: {
+              marginTop: 10,
+              marginBottom: 10,
+          }
+        },
+      };
+      
+    return (
         <View style={globalStyles.container}>
             <Text>Timer</Text>
-            <Text style={globalStyles.paragraph} onPress={() => navigation.navigate('Timer')}>Start Timer</Text>
-            <Text style={globalStyles.paragraph} onPress={() => navigation.navigate('Login', { screen: 'Login' } )}>LOG OUT</Text>
+            <ThemeProvider theme={theme}>
+                <Button title="1 hour" onPress={() => navigation.navigate('Timer', {cycle: 1} )}/>
+                <Button title="2 hours" onPress={() => navigation.navigate('Timer', {cycle: 2} )}/>
+                <Button title="3 hours" onPress={() => navigation.navigate('Timer', {cycle: 3} )}/>
+            </ThemeProvider>
+            <Text style={globalStyles.paragraph} onPress={() => navigation.navigate('Login', { screen: 'Login' })}>LOG OUT</Text>
         </View>
-        )
+    )
 }
--- screens/Timer.js
+++ screens/Timer.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { globalStyles} from '../styles/global';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
+import { View, Text, Animated } from 'react-native';
+import { globalStyles } from '../styles/global';
 
-export default function Timer() {
+export default function Timer( { route } ) {
+    const { cycle } = route.params; 
     return (
         <View style={globalStyles.container}>
             <Text>Timer page</Text>
+            <CountdownCircleTimer
+                onComplete={() => {
+                    // add to database the animal 
+                    return [true, 1000] // repeat animation in 1 seconds
+                }}
+                isPlaying
+                duration={10 * cycle}
+                colors={[
+                    ['#004777', 0.4],
+                    ['#F7B801', 0.4],
+                    ['#A30000', 0.2],
+                ]}
+            >
+                {({ remainingTime, animatedColor }) => {
+                    const minutes = Math.floor(remainingTime / 60);
+                    const seconds = remainingTime % 60;
+                    return (<Animated.Text style={{ color: animatedColor }}>
+                        {`${minutes}:${seconds}`}
+                    </Animated.Text>)
+                }}
+            </CountdownCircleTimer>
         </View>
     )
 }
